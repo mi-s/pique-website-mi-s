5438f1b4f4c4748b0c5d8f1a90b707965e44ee32
mi-s <michaelsu@uchicago.edu>
2019-09-11 18:08:30 -0500

    adding image carousel

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/* Mobile Menu Script */
 window.addEventListener("click", function(event) {
   if (event.target.matches("#openDropdown")) {
     document.getElementById("dropdownContent").classList.add("show");
@@ -9,3 +10,33 @@ window.addEventListener("click", function(event) {
     document.getElementById("openDropdown").classList.remove("hide");
   }
 });
+
+/* Slideshow Script */
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
